d8bf1842729bcd6518f5be902011ea63e8b9bee3
Kyndrael <jonathansilveira95@hotmail.com>
2025-06-24 18:14:54 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,1 +1,12 @@
 
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://seu-endpoint-api.com', // ou localhost se for local
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  projectId: 'xxxxxx' // substitua pelo ID do projeto gerado pela Cypress Cloud
+})
