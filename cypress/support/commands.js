70830b4ba996f2d8dfbc827212cfe22d242e506f
Kyndrael <jonathansilveira95@hotmail.com>
2025-06-24 18:14:17 -0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,11 @@
 
+// Você pode definir comandos customizados aqui, exemplo:
+Cypress.Commands.add('loginApi', (email, password) => {
+  cy.request({
+    method: 'POST',
+    url: '/login',
+    body: { email, password }
+  }).then((resp) => {
+    window.localStorage.setItem('token', resp.body.token)
+  })
+})
