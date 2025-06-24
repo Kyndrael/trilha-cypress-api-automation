a6a9b9fb7ee9da83722bdd07d93460cee9df23ce
Kyndrael <jonathansilveira95@hotmail.com>
2025-06-24 18:10:08 -0300

    Update examples.cy.js

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,1 +1,7 @@
+describe('Exemplo de Teste Cypress', () => {
+  it('Visita a página inicial e verifica o título', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Kitchen Sink')
+  })
+})
 
